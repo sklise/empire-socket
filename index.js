cb25a08fb0149006b0c3d014da26bb78282d73bc
Steve Klise <sk@skli.se>
2014-05-05 20:44:24 -0400

    more

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
   var sys = require('sys'),
     fs = require('fs'),
     gsock = '',
-    osc = require('node-osc');
-
-  m = '';
-
-  newmessage = false;
+    osc = require('node-osc'),
+    lodash = require('lodash');
 
   oscServer = new osc.Server(1337, '0.0.0.0');
 
